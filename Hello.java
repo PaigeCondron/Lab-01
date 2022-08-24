69ffad9102ee86000c6c837f2a57202a625ccb6e
PaigeCondron <112026126+PaigeCondron@users.noreply.github.com>
2022-08-24 17:09:02 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/************************************************************
+*Java Program Name 
+**************************************************************Student Name (Paige Condron)
+*Date Created (8/24/22)
+*Course Number and Section Number (CMSC-255-001)
+************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Paige>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Code is used to create words written in a specific order");
+		System.out.println("<8/24/22>");
+		System.out.println("<CMSC 255-001>");
+		System.out.println();
+	}
+}
+
+
